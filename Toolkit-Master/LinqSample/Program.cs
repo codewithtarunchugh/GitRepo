a033731c5d4f8ad09b57067d141da58a592aed1e
codewithtarunchugh <88618652+codewithtarunchugh@users.noreply.github.com>
2023-04-25 22:15:31 +0530

    update

--- Toolkit-Master/LinqSample/Program.cs
+++ Toolkit-Master/LinqSample/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
 // 1) Given an array of integers, write a LINQ query to find all the even numbers.
 // 2) Given a list of names, write a LINQ query to find all the names that start with the letter 'A'.
 // 3) Given a list of students with their grades, write a LINQ query to find the average grade of all the 
@@ -10,4 +10,24 @@ Console.WriteLine("Hello, World!");
 // 8) Given a list of strings, write a LINQ query to find all the strings that contain the substring "
 // 9) Given a list of integers, write a LINQ query to find the second smallest number in the list.
 // 10) Given a list of employees with their salaries, write a LINQ query to find the top 5 earners.
+
+int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var even = from i in a where i % 2 == 0 select i;
+var odd = from i in a where i % 2 == 1 select i;
+
+var even1 = a.Where(n => n % 2 == 0);
+var odd1 = a.Where(n => n % 2 == 1);
+
+Console.WriteLine("Even Numbers");
+foreach (var item in even1)
+{
+    Console.Write(item + ", ");
+}
+Console.WriteLine();
+Console.WriteLine("Odd numbers");
+foreach (var item in odd1)
+{
+    Console.Write(item + ", ");
+}
+
 
