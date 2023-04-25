4d2423ab660f6a02b3e0814e0276d3459c6a1708
codewithtarunchugh <88618652+codewithtarunchugh@users.noreply.github.com>
2023-04-25 21:49:03 +0530

    Question

--- Toolkit-Master/LinqSample/Program.cs
+++ Toolkit-Master/LinqSample/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// 1) Given an array of integers, write a LINQ query to find all the even numbers.
+// 2) Given a list of names, write a LINQ query to find all the names that start with the letter 'A'.
+// 3) Given a list of students with their grades, write a LINQ query to find the average grade of all the 
+// 4) Given a list of products with their price, write a LINQ query to find the most expensive product.
+// 5) Given a list of dates, write a LINQ query to find all the dates that are in the past week.
+// 6) Given a list of customers with their orders, write a LINQ query to find the total amount spent by
+// 7) Given a list of products with their categories, write a LINQ query to find the number of products
+// 8) Given a list of strings, write a LINQ query to find all the strings that contain the substring "
+// 9) Given a list of integers, write a LINQ query to find the second smallest number in the list.
+// 10) Given a list of employees with their salaries, write a LINQ query to find the top 5 earners.
+
