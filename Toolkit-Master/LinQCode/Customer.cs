4bc902ec8f988eef404df082bb6c67bbbe6222d2
codewithtarunchugh <88618652+codewithtarunchugh@users.noreply.github.com>
2023-04-26 21:05:44 +0530

    Linq Statements

---
+++ Toolkit-Master/LinQCode/Customer.cs
@@ -0,0 +1,10 @@
+﻿public class Customer
+{
+    public string? Name { get; set; }
+    public ICollection<Order>? Orders { get; set; }
+}
+public class Order
+{
+    public string? Product { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ Toolkit-Master/LinQCode/Employee.cs
@@ -0,0 +1,13 @@
+﻿
+public class Employee
+{
+    public string Name { get; set; }
+    public int Salary { get; set; }
+
+    public Employee(string name, int salary)
+    {
+        Name = name;
+        Salary = salary;
+    }
+}
+
